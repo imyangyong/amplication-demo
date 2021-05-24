9508f2b7d185e6a11b0bea8904dd0a0c5d7b8d50
杨勇 <1994101600@qq.com>
2021-05-24 11:49:02 +0800

    chore: passport http strategy annotation

--- server/src/auth/basic.strategy.ts
+++ server/src/auth/basic.strategy.ts
@@ -1,20 +1,21 @@
-import { BasicStrategy as Strategy } from "passport-http";
-import { PassportStrategy } from "@nestjs/passport";
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { UserInfo } from "./UserInfo";
+import { BasicStrategy as Strategy } from 'passport-http'
+import { PassportStrategy } from '@nestjs/passport'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { UserInfo } from './UserInfo'
 
 @Injectable()
 export class BasicStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super();
+    super()
   }
 
+  // passport-http[BasicStrategy] https://github.com/passport/express-3.x-http-basic-example/blob/master/server.js
   async validate(username: string, password: string): Promise<UserInfo> {
-    const user = await this.authService.validateUser(username, password);
+    const user = await this.authService.validateUser(username, password)
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException()
     }
-    return user;
+    return user
   }
 }
